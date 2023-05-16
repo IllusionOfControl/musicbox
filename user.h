3ece12d642b97a6a768d700265c3b6a0d0617cd7
User <User@192.168.248.129>
2023-05-16 18:42:28 +0300

    fix song. added song to eeprom

--- user.h
+++ user.h
@@ -6,43 +6,44 @@
 #define Empty   255
 
 
+
+__EEPROM_DATA(D3, P, F3, P, G3, G3, P, D3);
+__EEPROM_DATA(P, F3, P, G3d, G3, G3, P, P,);
+__EEPROM_DATA(D3, P, F3, P, G3, G3, P, F3); 
+__EEPROM_DATA(P, D3, D3, D3, D3, P, P, P, );
+__EEPROM_DATA(P, Empty, Empty, Empty, Empty, Empty, Empty, Empty);
+
 const unsigned char song[] = {
-    D3, D3, D3, D3, 
-    P, P, P, P,
-    F3, F3, F3, F3, 
-    P, P, P, P,
-    G3, G3, G3, G3,
-    G3, G3, G3, G3,
-    P, P, P, P,
+    D3,
+    P,
+    F3,
+    P,
+    G3, 
+    G3, 
+    P,
     
-    D3, D3, D3, D3, 
-    P, P, P, P,
-    F3, F3, F3, F3, 
-    P, P, P, P,
-    G3d, G3d, G3d, G3d,
-    G3, G3, G3, G3,
-    G3, G3, G3, G3,
-    P, P, P, P,
-    P, P, P, P,
+    D3, 
+    P, 
+    F3,
+    P,
+    G3d,
+    G3, 
+    G3, 
+    P, 
+    P, 
     
-    D3, D3, D3, D3, 
-    P, P, P, P,
-    F3, F3, F3, F3, 
-    P, P, P, P,
-    G3, G3, G3, G3,
-    G3, G3, G3, G3,
-    P, P, P, P,
-    F3, F3, F3, F3, 
-    P, P, P, P,
-    D3, D3, D3, D3, 
-    D3, D3, D3, D3, 
-    D3, D3, D3, D3, 
+    D3,
+    P, 
+    F3, 
+    P, 
+    G3, 
+    G3, 
+    P, 
+    F3, 
+    P,
     D3, D3, D3, D3, 
     
     P, P, P, P,
-    P, P, P, P,
-    P, P, P, P,
-    
 };
 
 void InitApp(void);
